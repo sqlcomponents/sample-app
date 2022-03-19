a1c6791e9fec6dab4bd89065786c82d4d9d751b2
Sathish Kumar Thiyagarajan <sathishk@users.noreply.github.com>
2022-03-19 13:06:58 +0530

    Updated with Sample CRUD

--- src/main/java/org/sqlcomponents/App.java
+++ src/main/java/org/sqlcomponents/App.java
@@ -22,15 +22,30 @@ public class App {
         // Lets get the store
         MovieStore movieStore = movieManager.getMovieStore();
 
+        Movie movie = new Movie();
+
+        movie.setRating(2.3);
+        movie.setGenre("Action");
+        movie.setTitle("Inception");
+        movie.setImdbId("23");
+        movie.setYearOfRelease(((short) 2301));
+        movie.setDirectedBy("Christopher Nolan");
+
+        Movie ceateMovie = movieStore.insert().values(movie).returning();
+
+        System.out.println("Movie Created " + ceateMovie.getId());
+
         // Select your data
         List<Movie> movies =  movieStore.select(
                 directedBy().eq("Christopher Nolan")
         );
 
         // Print it
-        movies.stream().forEach(movie -> {
-            System.out.println(movie.getTitle() + "(" + movie.getYearOfRelease() + ") - "+movie.getRating()+" - " +movie.getDirectedBy() );
+        movies.stream().forEach(m -> {
+            System.out.println(m.getTitle() + "(" + m.getYearOfRelease() + ") - "+m.getRating()+" - " +m.getDirectedBy() );
         });
+
+        movieStore.deleteAll();
 
     }
 
