e487e0bfa7d706648818de9853b1e965299b78b7
VIJAYWHAT <vijayrv042@gmail.com>
2022-06-18 15:57:02 +0530

    sketch for actor

---
+++ src/main/java/com/example/service/ActorService.java
@@ -0,0 +1,55 @@
+package com.example.service;
+
+import org.example.model.Actor;
+
+/**
+ * ActorService.
+ */
+public class ActorService {
+    /**
+     * create a Actor .
+     * @param username
+     * @param actor
+     * @return CreatedName
+     */
+    public Actor create(final String username,
+                        final Actor actor) {
+        return null;
+    }
+
+    /**
+     * Read Actor By id.
+     * @param username
+     * @param actorId
+     * @return actor
+     */
+    public Actor read(final String username,
+                        final Long actorId) {
+        return null;
+    }
+
+    /**
+     * Update Actor By id.
+     * @param username
+     * @param actorId
+     * @return ActorUpdated
+     */
+    public Actor update(final String username,
+                        final Long actorId) {
+        return null;
+    }
+
+    /**
+     * Delete Actor By id.
+     * @param username
+     * @param actorId
+     * @return IsDeleted
+     */
+    public Actor delete(final String username,
+                        final Long actorId) {
+        return null;
+    }
+
+
+
+}
---
+++ src/test/java/com/example/service/ActorServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActorServiceTest {
+
+    @Autowired
+    private ActorService actorService;
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
